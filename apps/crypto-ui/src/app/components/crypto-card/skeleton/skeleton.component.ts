cba3391d21a9d98136e2076d00df350255f61808
itsokto <36706257+itsokto@users.noreply.github.com>
2021-08-02 10:49:55 +0300

    crypto-card skeleton styling

--- apps/crypto-ui/src/app/components/crypto-card/skeleton/skeleton.component.ts
+++ apps/crypto-ui/src/app/components/crypto-card/skeleton/skeleton.component.ts
@@ -9,19 +9,21 @@ import { NgxSkeletonLoaderConfigTheme } from 'ngx-skeleton-loader/lib/ngx-skelet
 export class SkeletonComponent {
   priceTheme: NgxSkeletonLoaderConfigTheme = {
     width: '150px',
-    'margin-bottom': '0.5rem',
+    height: '1.1rem',
+    'margin-bottom': '0',
     background: 'rgba(255, 255, 255, 0.3)',
   };
 
   labelTheme: NgxSkeletonLoaderConfigTheme = {
     width: '120px',
+    height: '1.1rem',
     'margin-bottom': '0',
     background: 'rgba(255, 255, 255, 0.3)',
   };
 
   iconTheme: NgxSkeletonLoaderConfigTheme = {
-    width: '32px',
-    height: '32px',
+    width: '2rem',
+    height: '2rem',
     margin: '0',
     'margin-right': '0.5rem',
     background: 'rgba(255, 255, 255, 0.3)',
